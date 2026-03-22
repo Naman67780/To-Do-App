a03476d0af6971d1b5a7496e5e6c40bb8d83e8e1
Naman <namansaini1371@gmail.com>
2026-03-22 17:38:34 +0530

    Changed JS file

--- scripts.js
+++ scripts.js
@@ -1,85 +1,44 @@
-// TO-DO-App
-let addBox=document.getElementById("tasks")
-let addButton=document.getElementById("addButton")
-let searchBox=document.getElementById("search")
-let searchButton=document.getElementById("searchButton")
-let taskContainer=document.getElementById("taskContainer")
-let completedTask=document.getElementById("completed")
-let notCompletedTasks=document.getElementById("notcompleted")
+// TO-DO-APP
+//Selectors
+const inputBox=document.getElementById("inputTasks")
+const addButton=document.getElementById("addButton")
+const searchBox=document.getElementById("searchBox")
+const searchButton=document.getElementById("searchButton")
+const taskContainer=document.getElementById("taskContainer") 
 
-//Lists and Variables
-let tasks=[]
-let complet=0
-let notComplete=0
-let newdiv
-
+//Variables and Lists
+let taskList=[]
 //Functions
-function addText(){
-    let task=addBox.value
-    tasks.push(task)
-    console.log(task)
-
+function addTask(){
+    const task=inputBox.value
+    taskList.push(task)
 }
-function addTaskToList(){
-    let task=addBox.value
-    tasks.push(task)
-}
-
-function addTasks(){
-    let task=addBox.value
-    //creating elements
-    newdiv=document.createElement("div")
-    let para=document.createElement("p")
-    let completeBtn=document.createElement("button")
-    let tickImg=document.createElement("img")
-    let notCompleteBtn=document.createElement("button")
-    let crossImg=document.createElement("img")
-    //Inner Html
-    para.innerHTML=`${task}`
-    tickImg.src="https://cdn-icons-png.flaticon.com/128/7103/7103702.png"
-    crossImg.src="https://cdn-icons-png.flaticon.com/128/83/83972.png"
-    //Adding elements
-    completeBtn.appendChild(tickImg)
-    notCompleteBtn.appendChild(crossImg)
-    newdiv.appendChild(para)
-    newdiv.appendChild(completeBtn)
-    newdiv.appendChild(notCompleteBtn)
-    //Styles
+function displayTask(){
+    const task=inputBox.value
+    const para=document.createElement("p")
+    const tickButton=document.createElement("button")
+    const crossButton=document.createElement("button")
+    const tickImg=document.createElement("img")
+    const crossImg=document.createElement("img")
+    tickImg.src="https://cdn-icons-png.flaticon.com/128/9258/9258209.png"
+    crossImg.src="https://cdn-icons-png.flaticon.com/128/18962/18962545.png"
     tickImg.style.height="10px"
-    tickImg.style.width="10px"
     crossImg.style.height="10px"
-    crossImg.style.width="10px"
-    para.style.margin="auto"
-    para.style.flex="1"
-    para.style.border="1px solid black"
-    newdiv.style.display="flex"
-    // newdiv.style.border="1px solid black"
-    newdiv.style.marginTop="5px"
-    newdiv.style.gap="1px"
-    taskContainer.appendChild(newdiv)
-    //Event Listner
-    completeBtn.addEventListener("click",completedTasks)
-    notCompleteBtn.addEventListener("click",nonCompletedTasks)
-    notCompleteBtn.addEventListener("click",removeTask)
-    completeBtn.addEventListener("click",removeTask)
+    tickButton.appendChild(tickImg)
+    crossButton.appendChild(crossImg)
+    para.innerText=`${task}`
+    para.appendChild(tickButton)
+    para.appendChild(crossButton)
+    tickButton.style.marginLeft="10px"
+    taskContainer.appendChild(para)
+    //Inner Functions
+   
+}
+function removeImput(){
+    inputBox.value=""
 }
 
-
-function completedTasks(){
-    complet+=1
-    completedTask.innerText=`Completed: ${complet}`
-}
-
-function nonCompletedTasks(){
-    notComplete+=1
-    notCompletedTasks.innerText=`Not Completed: ${notComplete}`
-}
-function removeTask(){
-    let taskToremove=newdiv
-    newdiv.remove()
-}
-
-//Event Listeners
-addButton.addEventListener("click",addTasks)
-addButton.addEventListener("click",addTaskToList)
-
+//Event Listners
+addButton.addEventListener("click",displayTask)
+addButton.addEventListener("click",removeImput)
+console.log(taskList)
